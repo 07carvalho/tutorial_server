cb2c6a7c84af02ffaea99c5153d605012b985e36
Felipe Carvalho <felipeca@motorola.com>
2019-10-23 14:26:19 -0400

    model, views e template

--- dashboard/base/models.py
+++ dashboard/base/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Report(models.Model):
+
+    class Meta:
+        app_label = 'base'
+
+    name = models.CharField(max_length=32)
+    total_value = models.IntegerField()
+    disk_usage = models.FloatField()
+    latency = models.FloatField()
+    created = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return '{} - {}'.format(self.name, self.created)
--- dashboard/static/js/dashboard.js
+++ dashboard/static/js/dashboard.js
@@ -1,3 +1,24 @@
 $(document).ready(function() {
+    var ctx = document.getElementById('myChart');
+    console.log(REPORT_DATA);
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: REPORT_DATA.c,
+            datasets: [{
+                label: 'Total Values',
+                data: REPORT_DATA.d,
+            }]
+        },
+    });
 
+    var progValues = $(".progress-value");
+    progValues.each(function() {
+        var value = $(this).attr('data-value')
+        $(this).animate({width: value + '%'}, 10);
+    });
+
+    setTimeout(function() {
+        window.location.reload(true);
+    }, 300000);
 });
